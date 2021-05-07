e463045d4a02751720f9e5e031dde6bf478e9f55
(factory_yadon) <(yyeet@naver.com)>
2021-05-07 09:01:13 +0900

    office 210507 0901

---
+++ 5_frame.py
@@ -0,0 +1,100 @@
+import pygame
+
+pygame.init()                               # 초기화 반드시 필요
+
+# 화면크기 설정
+screen_width = 480
+screen_height = 640
+
+screen = pygame.display.set_mode((screen_width,screen_height))
+
+# 배경 이미지 불러오기
+background = pygame.image.load("D:\\1.프로젝트\\잡동사니\\python\\pygame\\pygame_basic\\background.png")
+
+# 캐릭터(스프라이트) 불러오기
+character = pygame.image.load("D:\\1.프로젝트\\잡동사니\\python\\pygame\\pygame_basic\\character.png")
+character_size = character.get_rect().size                                                                  # 이미지의 크기 구해옴
+character_width = character_size[0]                                                                         # 캐릭터의 width
+character_height = character_size[1]                                                                        # 캐릭터의 세로 크기
+character_x_pos = screen_width/2 -(character_width/2)                                                        # 캐릭터의 가로 위치
+character_y_pos = screen_height - character_height                                                          # 캐릭터의 세로 위치
+
+
+
+# 화면 타이틀 설정
+pygame.display.set_caption("Nado Game")
+
+# FPS
+clock = pygame.time.Clock()
+
+# 이동할 좌표
+to_x = 0 
+to_y = 0
+
+
+# 이동 속도
+
+character_speed = 0.3
+
+# 적 enemy 캐릭터
+
+
+
+# 이벤트 루프
+running = True
+while running:
+    dt = clock.tick(10)
+
+# 캐릭터가 1초동안에 100 만큼 이동함
+# 10fps : 1초동안에 10번 동작 -> 1번에 10만큼 이동
+# 20fps : 1초동안에 20번 동작 -> 1번에 5만큼 이동
+
+    print("fps = {0}".format(clock.get_fps()))
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                to_x -= character_speed
+            elif event.key == pygame.K_RIGHT:
+                to_x += character_speed
+            elif event.key == pygame.K_UP:
+                to_y -= character_speed
+            elif event.key == pygame.K_DOWN:
+                to_y += character_speed
+
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                to_x = 0
+            elif event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                to_y = 0
+
+
+
+    character_x_pos += to_x * dt
+    character_y_pos += to_y * dt
+
+    if character_x_pos < 0:
+        character_x_pos = 0
+    elif character_x_pos > screen_width - character_width:
+        character_x_pos = screen_width - character_width
+
+    # 세로 경계값 처리
+    if character_y_pos < 0:
+        character_y_pos = 0
+    elif character_y_pos > screen_height - character_height:
+        character_y_pos = screen_height - character_height
+    
+
+
+
+    screen.blit(background,(0,0))                               # 배경 그리기
+    screen.blit(character,(character_x_pos,character_y_pos))     # 캐릭터 그리기
+
+    pygame.display.update()                                     # 게임 화면 갱신
+
+
+# pygame 종료 처리
+pygame.QUIT()
