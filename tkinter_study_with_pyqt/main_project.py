82fc6b0e492ac8128899ea1d621fb202c0a71262
(factory_yadon) <(yyeet@naver.com)>
2021-05-21 12:39:53 +0900

    office 210521 1239

--- tkinter_study_with_pyqt/main_project.py
+++ tkinter_study_with_pyqt/main_project.py
@@ -2,6 +2,7 @@ import sys
 import os
 from PyQt5.QtWidgets import *
 from PyQt5 import uic
+from PyQt5.QtCore import pyqtSignal
 import signal_slot
 
 
@@ -11,18 +12,22 @@ form_class = uic.loadUiType(os.path.join(folder_path,"form/main_form.ui"))[0]
 
 #화면을 띄우는데 사용되는 Class 선언
 class WindowClass(QMainWindow, form_class) :
+    
     def __init__(self) :
         super().__init__()
         self.setupUi(self)
         sig_slot = signal_slot.initial_signal(self)
 
+    
+    # 이벤트 정리 쪽
+    
     def show_file_dialog(self):
-        frame = QFileDialog.getOpenFileName(self,'open file','./')
-        print(frame)
+        frame = QFileDialog.getOpenFileName(self,"open choose file","pygame_practice",filter="*.png")
+        print(frame[0])
+        self.tb_File_list.append(frame[0])
+
 
     
-   
-        
 
 
 if __name__ == "__main__" :
--- tkinter_study_with_pyqt/signal_slot.py
+++ tkinter_study_with_pyqt/signal_slot.py
@@ -1,12 +1,17 @@
 from PyQt5.QtWidgets import QFileDialog
+from PyQt5.QtCore import pyqtSignal
 
 class initial_signal():
 
-    def __init__(self,mainform):
-        mainform.btn_File_add.clicked.connect(self.show_file_dial(mainform))
 
-    def show_file_dial(self,mainform):
-        print("안녕하세요")
+
+    def __init__(self,mainform):
+        mainform.btn_File_add.clicked.connect(mainform.show_file_dialog)            # 파일 불러오기 Dialog open
+        mainform.btn_Close_app.clicked.connect(mainform.close)                      # main window close
+        
+    
+
+    
     
 
 
